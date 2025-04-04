c320669631aab232bda3ecc14347d293f7382856
tronaluz <lucas_tron@hotmail.com>
2025-04-03 22:34:29 -0300

    add Jest and MSW for testing; refactor store configuration and enhance Produtos tests

--- src/containers/Produtos/__tests__/Produtos.test.tsx
+++ src/containers/Produtos/__tests__/Produtos.test.tsx
@@ -1,9 +1,73 @@
-import Produtos from ".."
-import {renderizaComProvider} from '../../../utils/tests'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { screen, waitFor } from '@testing-library/react'
 
-describe("TResdtes para o container produtos", () => {
-    test('Deve renderizar corretamente', () => {
-       const { debug } =  renderizaComProvider(<Produtos />)
-        debug()
+import Produtos from ".."
+import { renderizaComProvider } from '../../../utils/tests'
+
+
+
+const mocks = [
+    {
+        id: 1,
+        categoria: 'RPG',
+        imagem: '',
+        plataformas: ['Windows'],
+        preco: 150.9,
+        precoAntigo: 199.9,
+        titulo: 'Elden RIng'
+    },
+    {
+        id: 2,
+        categoria: 'RPG',
+        imagem: '',
+        plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+        preco: 199.9,
+        precoAntigo: 299.9,
+        titulo: 'Hogwarst Legacy'
+    },
+    {
+        id: 3,
+        categoria: 'Acao',
+        imagem: '',
+        plataformas: ['PS5', 'Xbox Series S/X'],
+        preco: 150,
+        precoAntigo: 200,
+        titulo: 'Gotham Knights'
+    },
+    {
+        id: 4,
+        categoria: 'Aventura',
+        imagem: '',
+        plataformas: ['Nintendo Switch'],
+        preco: 189.9,
+        precoAntigo: 299.9,
+        titulo: 'Donkey Kong Country Tropical Freeze'
+    }
+]
+
+const server = setupServer(
+    rest.get('http://localhost:4000/produtos', 
+        (requisicao, resposta, contexto) => {
+        return resposta(contexto.json(mocks))
+    })
+)
+
+describe('Testes para o container produtos', () => {
+    beforeAll(() => server.listen())
+    afterEach(() => server.resetHandlers())
+    afterAll(() => server.close())
+
+    test('Deve renderizar corretamente com o texto de carregando', () => {
+        renderizaComProvider(<Produtos />)
+        expect(screen.getByText('Carregando...')).toBeInTheDocument()
+    })
+
+    test('Deve renderizar corretamente com a listagem de jogos', async() => {
+        const { debug } = renderizaComProvider(<Produtos />)
+        await waitFor(() => {
+            debug()
+            expect(screen.getByText('Donkey Kong Country Tropical Freeze')).toBeInTheDocument()
+        })
     })
 })
